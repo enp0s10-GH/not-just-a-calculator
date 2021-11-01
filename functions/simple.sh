4c171bacbbe71caa989238a52a59eb22a75ce9ca
Vincent Roch <vincent.roch@hetzner.com>
2021-11-01 08:01:20 +0100

    Formatted Code

--- functions/simple.sh
+++ functions/simple.sh
@@ -10,20 +10,20 @@ function simple() {
 
 # - This Function handles the simple calculation method - #
 function calculate() {
-num1=$1;
-op=$2;
-num2=$3;
-if [[ $num1 =~ $re ]] && [[ $num2 =~ $re ]]
-then
-case $op in
-"+") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1+num2)); echo -e -n "${Color_Off}";;
-"-") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1-num2)); echo -e -n "${Color_Off}";;
-"/") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1/num2)); echo -e -n "${Color_Off}";;
-"*") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1*num2)); echo -e -n "${Color_Off}";;
-"%") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1%num2)); echo -e -n "${Color_Off}";;
-*) echo -e "${BWhite}Falsche Rechenoperation ${BRed}${op}${BWhite} Verfügbar sind: ( ${BGreen}+ ${Color_Off}| ${BGreen}- ${Color_Off}| ${BGreen} ${Color_Off}| ${BGreen}/ ${Color_Off}| ${BGreen}% ${BWhite})"; echo -e -n "${Color_Off}";;
-esac
-else
-echo -e "${BRed}Das sind keine Zahlen. ( ͡❛  ͟ʖ ͡❛)" ; echo -e -n "${Color_Off}"
-fi
+    num1=$1;
+    op=$2;
+    num2=$3;
+    if [[ $num1 =~ $re ]] && [[ $num2 =~ $re ]]
+    then
+        case $op in
+            "+") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1+num2)); echo -e -n "${Color_Off}";;
+            "-") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1-num2)); echo -e -n "${Color_Off}";;
+            "/") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1/num2)); echo -e -n "${Color_Off}";;
+            "*") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1*num2)); echo -e -n "${Color_Off}";;
+            "%") echo -e -n "${BWhite}Das Ergebnis ist: "; echo -e -n "${BGreen}";echo $((num1%num2)); echo -e -n "${Color_Off}";;
+            *) echo -e "${BWhite}Falsche Rechenoperation ${BRed}${op}${BWhite} Verfügbar sind: ( ${BGreen}+ ${Color_Off}| ${BGreen}- ${Color_Off}| ${BGreen} ${Color_Off}| ${BGreen}/ ${Color_Off}| ${BGreen}% ${BWhite})"; echo -e -n "${Color_Off}";;
+        esac
+    else
+        echo -e "${BRed}Das sind keine Zahlen. ( ͡❛  ͟ʖ ͡❛)" ; echo -e -n "${Color_Off}"
+    fi
 }
